eb43b6de128daa555bd3b22b0f4cfa391c342a9f
CapybaraPin <me@hugo-robles.com>
2023-05-20 00:10:58 +0200

    Update main.py
    Ajouts de fonctionnalités comme : 
    
    - La winrate
    
    Et optimisation du code.

--- main.py
+++ main.py
@@ -14,32 +14,44 @@ pygame.display.set_caption("Jeu de Nim")
 circles = [(80*i, 200, 25) for i in range(1, 9)]
 
 # Définition des possibilités
-
 L = []
+
 for y in range(8):
     L.append([])
     for z in range(1,4):
         L[y].append(z)
 
-L = [[1,2,3], [2,3], [3], [], [1], [1,2,3], [1,2,3], [1,2,3]]
+#L = [[1,2,3], [2,3], [3], [], [1], [1,2,3], [1,2,3], [1,2,3]]
 
 # Définition des couleurs
-yellow = (226, 207, 16)
-red = (255, 0, 0)
+yellow = (232, 203, 53)
+red = (255, 87, 51)
 
 # Fonction permettant l'affichage de texte
-def displayText(texte, x, y, size):
+def displayText(text, x, y, size):
     """
     L'affichage d'un texte sur pygame étant redondant, cette fonction simplifie fortement la tache.
+
+    Paramètres : 
+        - text:str: Ce que vous voulez afficher comme texte
+        - x:int: La coordonnée en x de votre texte
+        - y:int: La coordonnée en y de votre texte
+        - size:int: La taille de votre texte
     """
 
     font = pygame.font.Font('Inter-Medium.ttf', size)
-    texte_affiche = font.render(texte, True, (0, 0, 0))
-    texte_rect = texte_affiche.get_rect()
-    texte_rect.center = (x, y)
-    screen.blit(texte_affiche, texte_rect)
+    text_display = font.render(text, True, (0, 0, 0))
+    text_rect = text_display.get_rect()
+    text_rect.center = (x, y)
+    screen.blit(text_display, text_rect)
 
 def possbilityList(L):
+    """
+        Affiche au dessus des cercles les possibilités que l'odinateur peut encore jouer.
+
+        Paramètres :
+            - L:list: La liste de liste comportant les possibilités.
+    """
 
     for i in range(len(L)):
 
@@ -48,13 +60,24 @@ def possbilityList(L):
             displayText(str(L[i][k]), 80+(i*80), 100+(k*20), 15)
 
 def reload():
+    """
+        Permet de recommencer une partie en rechargant les cercles.
+
+    :return: la liste des cercles
+    """
+
     circles = [(80 * i, 200, 25) for i in range(1, 9)]
 
     return circles
 
-
 actions = ""
-precedent = None
+last = None
+
+
+win_rate = 0
+total_games = 0
+
+# Boucle principale
 active = True
 while active:
 
@@ -63,10 +86,13 @@ while active:
         if event.type == pygame.QUIT:
             active = False
 
-    screen.fill((255, 255, 255))
+    # Affichage du fond en blanc
+    screen.fill((249, 249, 233))
 
     # Dessin des cercles
     num_red_circles = 0
+    
+    displayText("Machine : " + str(win_rate) + " / " + str(total_games), 60, 20, 15)
 
     for i, circle in enumerate(circles):
 
@@ -87,48 +113,64 @@ while active:
 
                 if pygame.mouse.get_pressed()[0] == 1:
                     actions = f"Vous avez retiré {num_red_circles} jetons."
-
+                    
+                    # Si le joueur n'a pas gagné alors on retire le montant de jetons qu'il a sélectionné
                     if num_red_circles < len(circles):
                         for i in range(num_red_circles):
                             circles.pop()
 
                         # La machine joue
                         indice = len(circles)-1
-
+                        
+                        # Vérifie que la liste n'est pas vide
                         if len(L[indice]) == 0:
 
-                            if L[precedent[0]] != []:
-                                L[precedent[0]].remove(precedent[1])
+                            # Vérifie que la liste de la dernière action n'est pas vide
+                            if L[last[0]] != []:
+                                
+                                # On supprime le dernier élement joué par la machine qui amène à la liste vide
+                                L[last[0]].remove(last[1])
 
                             actions = f"Bravo : Vous avez gagné ! (Ordi tombé sur L4)"
-
+                            total_games += 1                               
+                            
+                            # On relance une partie
                             circles = reload()
 
+                        
                         else:
+                            # On pioche parmis les possibilités
+                            n = random.choice(L[indice])                                
+                                
+                            last = indice, n
 
-                            if len(L[indice]) == 1:
-                                n = L[indice][0]
-
-                            elif len(L[indice]) == 2:
-                                n = random.choice([L[indice][0],L[indice][1]])
-
-                            elif len(L[indice]) == 3:
-                                n = random.randint(1,3)
-
-                            precedent = indice, n
-
+                            # Si la machine ne gagne pas alors on retire les jetons
                             if n < len(circles):
                                 for i in range(n):
                                     circles.pop()
+                                    
                                 actions = f"La machine retire {n} jetons!"
 
+                            # Sinon la machine à gagnée
                             elif n >= len(circles):
                                 actions = f"Dommage ! La machine a gagnée!"
+                                
+                                total_games += 1
+                                win_rate += 1
+                                
+                                # On relance une partie
                                 circles = reload()
-
+                                
+                    # Sinon je joueur à gagné
                     elif num_red_circles >= len(circles):
+                        
                         actions = f"Bravo : Vous avez gagné !"
+                        total_games += 1
+                               
+                        # On supprime le dernier coup de la machine
                         L[indice].remove(n)
+                        
+                        # On relance la partie
                         circles = reload()
 
                 break
@@ -138,8 +180,10 @@ while active:
             # Si la souris n'est pas sur le cercle, tous les cercles sont jaunes
             pygame.draw.circle(screen, yellow, circle[:2], circle[2])
 
+    # Affichage des différentes actions et messages
     displayText(actions, 720 / 2, 350, 20)
 
+    # Affichage de la liste des possibilités
     possbilityList(L)
 
     pygame.display.flip()
